f06372ca673710a115b06071e1edb3523685ef78
vitorotto <vitorotto05@gmail.com>
2025-07-15 10:57:18 -0300

    Parte01: Declaração de Variáveis

---
+++ CursoDeJava/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // TIPO DE DADO + IDENTIFICAÇÃO + VALOR
+        String nome = "Vitor Hugo Otto";
+        System.out.println(nome);
+    }
+}
