09c85ba7baede99dc359ef50a88ed8d44d88b4da
vitorotto <vitorotto05@gmail.com>
2025-09-20 18:08:22 -0300

    Ex010 concluido

---
+++ Logica/Aula02/Ex010.java
@@ -0,0 +1,18 @@
+/*
+* Faça um algoritmo que leia uma temperatura em graus Celsius e apresente-a
+* convertida em graus Fahrenheit. A fórmula de conversão é: F = (9 * C + 160) / 5, na
+* qual F é a temperatura em Fahrenheit e C é a temperatura em Celsius;
+*/
+
+import java.util.Scanner;
+
+public class Ex010 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Informe a temperatura em C°: ");
+        float celcius = s.nextFloat();
+
+        // Calculando e exibindo os resultados
+        System.out.println("fahrenheit: " + ((9 * celcius + 160)) / 5);
+    }
+}
