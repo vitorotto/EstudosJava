f4e81692e9d6e4a92e6d448f44fb321b20cfc727
vitorotto <vitorotto05@gmail.com>
2025-09-21 11:11:02 -0300

    Ex013 concluido

---
+++ Logica/Aula02/Ex013.java
@@ -0,0 +1,28 @@
+/*
+Faça um programa que leia dois valores para as variáveis A e B e efetue a troca
+dos valores de forma que a variável A passe a possuir o valor da variável B e a
+variável B passe a possuir o valor da variável A. Apresente os valores trocados.
+*/
+
+import java.util.Scanner;
+
+public class Ex013 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Informe a variável A: ");
+        int a = s.nextInt();
+
+        System.out.println("Informe a variável B: ");
+        int b = s.nextInt();
+
+        System.out.println("Valores antes da troca: A = " + a + " B = " + b);
+
+        int aux = a;
+        a = b;
+        b = aux;
+
+        System.out.println("Valores trocados: A = " + a + " B = " + b);
+
+    }
+}
