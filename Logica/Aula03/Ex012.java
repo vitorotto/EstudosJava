a545f225a0db9ecd4751ad6603fd1066f35c2c04
vitorotto <vitorotto05@gmail.com>
2025-10-03 12:34:34 -0300

    Ex012 iniciado

---
+++ Logica/Aula03/Ex012.java
@@ -0,0 +1,15 @@
+/*
+Alguns países medem temperaturas em graus Celsius, e outros em graus Fahrenheit.
+Crie um programa que pergunte se o usuário deseja converter a temperatura de oC para
+oF (lendo o número 1) e de oF para oC (lendo o número 2) depois de lida a opção do
+usuário, leia temperatura, converta de acordo com a opção do usuário e mostre o
+resultado para o usuário.
+*/
+import java.util.Scanner;
+
+public class Ex012 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+    }
+}
