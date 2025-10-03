535c6a300d8cf9cee4d42480ce4a11a84a328099
vitorotto <vitorotto05@gmail.com>
2025-10-02 23:36:59 -0300

    Ex011 iniciado

---
+++ Logica/Aula03/Ex011.java
@@ -0,0 +1,18 @@
+/*
+ * Faça um programa para ler: número da conta do cliente, saldo, débito e crédito. Após,
+calcular e escrever o saldo atual (saldo atual = saldo - débito + crédito). Também testar
+se saldo atual for maior ou igual a zero escrever a mensagem 'Saldo Positivo', senão
+escrever a mensagem 'Saldo Negativo'.
+ */
+import java.util.Scanner;
+
+public class Ex011 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Número da conta: ");
+        int numConta = s.nextInt();
+
+        
+    }
+}
