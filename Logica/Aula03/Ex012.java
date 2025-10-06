e0dcfc746158be5773885c4a21115f5ade0e18d5
vitorotto <vitorotto05@gmail.com>
2025-10-06 12:43:39 -0300

    Ex012 concluído

--- Logica/Aula03/Ex012.java
+++ Logica/Aula03/Ex012.java
@@ -1,15 +1,38 @@
-/*
-Alguns países medem temperaturas em graus Celsius, e outros em graus Fahrenheit.
-Crie um programa que pergunte se o usuário deseja converter a temperatura de oC para
-oF (lendo o número 1) e de oF para oC (lendo o número 2) depois de lida a opção do
-usuário, leia temperatura, converta de acordo com a opção do usuário e mostre o
-resultado para o usuário.
-*/
 import java.util.Scanner;
 
 public class Ex012 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
+        byte opc;
+        float temp;
+
+        System.out.println(
+                """
+                    Para qual temperatura deseja converter?
+                    Celsius - 1
+                    Fahrenheit - 2
+                """
+        );
+        opc = s.nextByte();
+
+        System.out.println("Informe a temperatura: ");
+        temp = s.nextFloat();
+
+        switch (opc) {
+            case 1:
+                // F para C
+                // (32 °F − 32) × 5/9 = 0 °C
+                System.out.println("F para C: " + ((temp - 32) * 5 / 9));
+                break;
+
+            case 2:
+                // C para F
+                System.out.println("C para F: " + ((temp * 9 / 5) + 32));
+                break;
+
+            default:
+                System.out.println("Algo inesperado aconteceu...");
+        }
     }
 }
