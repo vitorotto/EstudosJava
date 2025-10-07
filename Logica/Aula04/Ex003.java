452ec241681c7eb3b7bd75c42dbe995250884ca8
vitorotto <vitorotto05@gmail.com>
2025-10-06 22:45:53 -0300

    Ex 01 a 04

---
+++ Logica/Aula04/Ex003.java
@@ -0,0 +1,25 @@
+/*
+* Escreva um programa para ler um valor entre 1 (inclusive) e 10 (inclusive). Se o valor lido não estiver entre 1
+* (inclusive) e 10 (inclusive), deve ser lido um novo valor. Após a leitura do valor, escrever o valor lido na tela.
+*/
+
+import java.util.Scanner;
+
+public class Ex003 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        boolean pass = false;
+        int val = 0;
+        while (!pass) {
+            System.out.println("Informe um valor entre 1 e 10");
+            val = s.nextInt();
+            
+            if (val >= 1 && val <= 10) {
+                pass = true;
+            } else {
+                System.out.println("Valor fora do escopo, insira o valor novamente.");
+            }
+        }
+        System.out.println(val);
+    }
+}
