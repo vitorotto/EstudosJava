1a037d337ba22f0a0ac9c710ef1e0cf0b1aa51e7
vitorotto <vitorotto05@gmail.com>
2025-12-02 16:09:15 -0300

    test: teste do controller que insere no inicio e no final da lista

--- Logica/ExerciciosGemini/dynamic_playlist/src/test/java/controllers/PlaylistControllerTest.java
+++ Logica/ExerciciosGemini/dynamic_playlist/src/test/java/controllers/PlaylistControllerTest.java
@@ -14,10 +14,19 @@ public class PlaylistControllerTest {
         controller.addSongFirstController("Teste", "03:55");
         controller.addSongFirstController("Teste1", "03:55");
         controller.addSongFirstController("Teste2", "03:55");
+
+        controller.addSongLastController("Teste add final", "03:44");
+        controller.addSongLastController("Teste add final 1", "03:44");
+        controller.addSongLastController("Teste add final 2", "03:44");
+        controller.addSongLastController("Teste add final 3", "03:44");
+
         controller.addSongFirstController("Teste3", "03:55");
         controller.addSongFirstController("Teste4", "03:55");
         controller.addSongFirstController("Teste5", "03:55");
-        System.out.println(controller.getPlaylistData());
+
+        for (Object song : controller.getPlaylistData()) {
+            System.out.println(song);
+        }
 
     }
 }
