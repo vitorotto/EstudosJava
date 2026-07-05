619184738a4c773123d4ec8173b15eff0bed62b1
vitorotto <vitorotto05@gmail.com>
2026-07-04 21:12:12 -0300

    Add: Primeiro commit do projeto para estudar sobre a conexão do Java com banco MySQL usando JPA

---
+++ conexao_db/src/main/java/com/vitorotto/conexao_db/ConexaoDbApplication.java
@@ -0,0 +1,13 @@
+package com.vitorotto.conexao_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexaoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexaoDbApplication.class, args);
+	}
+
+}
---
+++ conexao_db/src/test/java/com/vitorotto/conexao_db/ConexaoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorotto.conexao_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConexaoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
