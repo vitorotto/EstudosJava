cd7338adeb97f97c5484cdec0aa517271e3adfbd
vitorotto <vitorotto05@gmail.com>
2026-07-18 21:08:21 -0300

    Fix: Override of "equals" and "hashCode" plus refactoring of the code for English lang.

---
+++ processamento_ecomerce/src/main/java/com/vitorotto/model/ClientModel.java
@@ -0,0 +1,91 @@
+package com.vitorotto.model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class ClientModel {
+
+    public enum ClientType {
+        VIP,
+        COMUM
+    }
+
+    private final int id;
+    private String name;
+    private ClientType type;
+    private String city;
+    private String state;
+    private final LocalDate registrationDate;
+
+    public ClientModel(int id, String name, ClientType type, String city, String state, LocalDate registrationDate) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.city = city;
+        this.state = state;
+        this.registrationDate = registrationDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ClientType getType() {
+        return type;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(ClientType type) {
+        this.type = type;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientModel [getId()=" + getId() + ", getName()=" + getName() + ", getType()=" + getType()
+                + ", getCity()=" + getCity() + ", getState()=" + getState() + ", getRegistrationDate()="
+                + getRegistrationDate() + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        ClientModel other = (ClientModel) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+}
