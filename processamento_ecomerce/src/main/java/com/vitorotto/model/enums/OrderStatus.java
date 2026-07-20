6bd19604301371b4e059c90dde1956446e15b191
vitorotto <vitorotto05@gmail.com>
2026-07-20 20:17:06 -0300

    Add: Enums to payment methods and order status.

---
+++ processamento_ecomerce/src/main/java/com/vitorotto/model/enums/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.vitorotto.model.enums;
+
+public enum OrderStatus {
+    PENDENTE,
+    CANCELADO,
+    PAGO;
+}
---
+++ processamento_ecomerce/src/main/java/com/vitorotto/model/enums/PaymentMethod.java
@@ -0,0 +1,8 @@
+package com.vitorotto.model.enums;
+
+public enum PaymentMethod {
+    CARTAO_CREDITO,
+    BOLETO,
+    PIX,
+    CARTAO_DEBITO;
+}
