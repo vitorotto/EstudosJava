37d7b78de6d448a3805dce30d247b5237bf6230b
vitorotto <vitorotto05@gmail.com>
2026-07-20 20:13:06 -0300

    Fix: ClientType changed to don't use codes.

--- processamento_ecomerce/src/main/java/com/vitorotto/model/enums/ClientType.java
+++ processamento_ecomerce/src/main/java/com/vitorotto/model/enums/ClientType.java
@@ -1,17 +1,6 @@
 package com.vitorotto.model.enums;
 
 public enum ClientType {
-    VIP(1),
-    COMUM(2);
-
-    private final int cod;
-
-    ClientType(int cod) {
-        this.cod = cod;
-    }
-
-    public int getCod() {
-        return cod;
-    }
-    
+    VIP,
+    COMUM;
 }
