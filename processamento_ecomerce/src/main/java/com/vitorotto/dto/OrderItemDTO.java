6fd98018c19c9176daa08d843c9ee259b0580b42
vitorotto <vitorotto05@gmail.com>
2026-07-25 07:56:08 -0300

    Add: OrderItemDTO class that will receive the data for each order item.

---
+++ processamento_ecomerce/src/main/java/com/vitorotto/dto/OrderItemDTO.java
@@ -0,0 +1,27 @@
+package com.vitorotto.dto;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class OrderItemDTO {
+    private final int productId;
+    private final int quantity;
+
+    @JsonCreator
+    public OrderItemDTO(
+        @JsonProperty("produtoId")
+        int productId,
+        @JsonProperty("quantidade")
+        int quantity
+    ) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+}
