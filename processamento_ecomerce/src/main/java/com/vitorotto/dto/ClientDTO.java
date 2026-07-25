ecd897c66fab9663cec7653938b6e33e5876d05c
vitorotto <vitorotto05@gmail.com>
2026-07-25 08:09:31 -0300

    feat: ClientDTO that will receive data from field "clientes".

---
+++ processamento_ecomerce/src/main/java/com/vitorotto/dto/ClientDTO.java
@@ -0,0 +1,62 @@
+package com.vitorotto.dto;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ClientDTO {
+    private final int id;
+    private final String name;
+    private final String type;
+    private final String city;
+    private final String state;
+    private final LocalDate registrationDate;
+
+    @JsonCreator
+    public ClientDTO(
+        @JsonProperty("id")
+        int id,
+        @JsonProperty("nome")
+        String name,
+        @JsonProperty("tipo")
+        String type,
+        @JsonProperty("cidade")
+        String city,    
+        @JsonProperty("estado")
+        String state,
+        @JsonProperty("dataCadastro")
+        LocalDate registrationDate
+    ) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.city = city;
+        this.state = state;
+        this.registrationDate = registrationDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+}
