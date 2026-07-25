888645e521ca32b9484162128c72116560d8fac7
vitorotto <vitorotto05@gmail.com>
2026-07-25 15:45:42 -0300

    test: Testing RootDTO, ClientMapper and ClientModel classes.

--- processamento_ecomerce/src/main/java/com/vitorotto/App.java
+++ processamento_ecomerce/src/main/java/com/vitorotto/App.java
@@ -1,11 +1,15 @@
 package com.vitorotto;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vitorotto.dto.OrderItemDTO;
+import com.vitorotto.dto.RootDTO;
+import com.vitorotto.mapper.ClientMapper;
+import com.vitorotto.model.ClientModel;
 
 /**
  * Hello world!
@@ -40,6 +44,24 @@ public class App
             System.err.println("Erro ao processar o JSON recebido");
             e.printStackTrace();
         }
+
+        System.out.println("\nTeste do ClientMapper");
+        strJson = "{\"clientes\": [{ \"id\": 1, \"nome\": \"Ana Beatriz Souza\", \"tipo\": \"VIP\", \"cidade\": \"Porto Alegre\", \"estado\": \"RS\", \"dataCadastro\": \"2022-03-14\" }, { \"id\": 2, \"nome\": \"Carlos Eduardo Lima\", \"tipo\": \"COMUM\", \"cidade\": \"Novo Hamburgo\", \"estado\": \"RS\", \"dataCadastro\": \"2023-07-01\" }],\"produtos\":[],\"pedidos\":[]}";
+        try {            
+            RootDTO rootDTO = mapper.readValue(strJson, RootDTO.class); // Read the json file
+            ClientMapper clientMapper = new ClientMapper(rootDTO.getClientList()); // Creates a clientMapper using the client list from root dto
+            List<ClientModel> clientList = clientMapper.mapAll().getSuccessItemsList(); // Create a list os client models with the success items list that can be returned by the MappingResult
+            for (ClientModel clientModel : clientList) {
+                System.out.println(clientModel.toString());
+            }      
+        } catch (JsonMappingException e) {
+            System.err.println("Erro ao ler o JSON");
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+            System.err.println("Erro ao processar o JSON recebido");
+            e.printStackTrace();
+        }
+        
     }
 
 }
